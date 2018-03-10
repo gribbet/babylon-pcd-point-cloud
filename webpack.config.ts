5f4ff270663eb604b23e806a53aa5b9e96b5b646
Graham <graham.gibbons@gmail.com>
2018-03-10 14:44:32 -0800

    Project setup

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as webpack from "webpack";
+
+const configuration: webpack.Configuration = {
+    context: `${__dirname}/source`,
+    entry: "./index.ts",
+    output: {
+        path: `${__dirname}/build`,
+        publicPath: "/"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: "ts-loader"
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
+
+export default configuration;
