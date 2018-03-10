685d60f8ce5d7308f14712f1380fc94f77a32d1d
Graham <graham.gibbons@gmail.com>
2018-03-10 15:17:01 -0800

    Add orbit controls

--- source/index.ts
+++ source/index.ts
@@ -1,24 +1,38 @@
-import * as three from "three";
+import * as THREE from "three";
+import { OrbitControls } from "three-orbitcontrols-ts";
 
-const scene = new three.Scene();
+const scene = new THREE.Scene();
 
-const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
-
-const renderer = new three.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setClearColor("#000000");
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 4;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
 document.body.appendChild(renderer.domElement);
 
-const geometry = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({ color: "#433F81" });
-const cube = new three.Mesh(geometry, material);
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
 
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: "#433F81" })
+);
 scene.add(cube);
 
-const render = function () {
+const render = () => {
     requestAnimationFrame(render);
 
     cube.rotation.x += 0.01;
