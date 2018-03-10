d35e44133207185adc9232d33016936d4ca2459d
Graham <graham.gibbons@gmail.com>
2018-03-10 14:48:44 -0800

    Basic three

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,30 @@
-console.log("Test");
+import * as three from "three";
+
+const scene = new three.Scene();
+
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 4;
+
+const renderer = new three.WebGLRenderer({ antialias: true });
+
+renderer.setClearColor("#000000");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new three.BoxGeometry(1, 1, 1);
+const material = new three.MeshBasicMaterial({ color: "#433F81" });
+const cube = new three.Mesh(geometry, material);
+
+scene.add(cube);
+
+const render = function () {
+    requestAnimationFrame(render);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+render();
